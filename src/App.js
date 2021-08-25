ecf0d68d5a572c33295c6f7eaac0382b6b4896f8
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2021-08-25 11:32:20 -0300

    probando selector ant

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React from 'react';
-import { DatePicker } from 'antd';
+import './index.css';
+import { Select } from 'antd';
 import 'antd/dist/antd.css';
+
+const { Option } = Select;
+function onChange(value) {
+  console.log(`selected ${value}`);
+}
+
+function onBlur() {
+  console.log('blur');
+}
+
+function onFocus() {
+  console.log('focus');
+}
+
+function onSearch(val) {
+  console.log('search:', val);
+}
 
 function App() {
   return (
     <>
-    </>
+      <div className="contenedor">
+        <Select
+          showSearch
+          style={{ width: 200 }}
+          placeholder="Select a person"
+          optionFilterProp="children"
+          onChange={onChange}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          onSearch={onSearch}
+          filterOption={(input, option) =>
+            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+          }
+        >
+          <Option value="jack">Jack</Option>
+          <Option value="lucy">Lucy</Option>
+          <Option value="tom">Tom</Option>
+        </Select>
+      </div>
+  </>
   );
 }
 
