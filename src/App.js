3a06e0b7b1a68a1a2338fce117bbbd79b8fff088
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2021-09-13 22:24:31 -0300

    se creo archivo seleccionado.js y modifico archivo index.css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import './index.css';
 import Seleccion from './componentes/seleccion';
+import Seleccionado from './componentes/seleccionado';
+
 
 function App() {
   return (
     <>
-      <div className="contenedor">
+      <div className='contenedor'>
+        <div className="contenedor_seleccion">
 
-        <Seleccion />
+          <Seleccion />
+          <Seleccionado/>
 
+        </div>
+
+
+
+        <div className='contenedor_circulos'>
+        <div id='circulo1'></div>
+        <div id='circulo2'></div>
+        <div id='circulo3'></div>
+        <div id='circulo4'></div>
+        <div id='circulo5'></div>
+        <div id='circulo6'></div>
+        <div id='circulo7'></div>
+        <div id='circulo8'></div>
+        </div>
       </div>
-
-      <div id='circulo1'></div>
-      <div id='circulo2'></div>
-      <div id='circulo3'></div>
-      <div id='circulo4'></div>
-      <div id='circulo5'></div>
-      <div id='circulo6'></div>
-      <div id='circulo7'></div>
-      <div id='circulo8'></div>
-
-  </>
+    </>
   );
 }
 
---
+++ src/componentes/seleccionado.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from 'antd';
+
+const Seleccionado = (props) => {
+    return (
+        <div>
+            <Button id='uno'>ID 1</Button>
+            <Button id='dos'>ID 2</Button>
+            <Button id='tres'>ID 3</Button>
+        </div>
+    );
+}
+
+
+export default Seleccionado;
